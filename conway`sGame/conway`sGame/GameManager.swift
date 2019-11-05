1cef292b99bff60ae875928a85130a79cc582c32
Cassia <cassia.a.barbosa@gmail.com>
2019-11-05 17:48:54 -0300

    Bronze and Silver done

---
+++ conway`sGame/conway`sGame/GameManager.swift
@@ -0,0 +1,9 @@
+//
+//  GameManager.swift
+//  conway`sGame
+//
+//  Created by Cassia Aparecida Barbosa on 05/11/19.
+//  Copyright © 2019 teamconwaysGame. All rights reserved.
+//
+
+import Foundation
---
+++ conway`sGame/conway`sGame/UI/Colors.swift
@@ -0,0 +1,9 @@
+//
+//  Colors.swift
+//  conway`sGame
+//
+//  Created by Cassia Aparecida Barbosa on 05/11/19.
+//  Copyright © 2019 teamconwaysGame. All rights reserved.
+//
+
+import Foundation
